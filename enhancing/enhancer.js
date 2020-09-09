43b5c8c99cd281e23394a3817266e2bd7c2b7d68
Josh Riddle <riddlej9879@yahoo.com>
2020-09-09 17:17:25 -0500

    MVP submission

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,35 @@ module.exports = {
 };
 
 function success(item) {
+  const maxEnhancement = 20;
+  if (item.enhancement === maxEnhancement) {
+    return { ...item };
+  } else if (item.enhancement < maxEnhancement) {
+    item.enhancement++;
+  } else {
+    item.enhancement = 0;
+  }
+
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 5) {
+    item.durability = item.durability - 5;
+  } else if (item.enhancement < 16) {
+    item.durability = item.durability - 15;
+  } else if (item.enhancement > 16) {
+    item.enhancement--;
+  }
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
+// Stretch goal
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,51 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer", () => {
+  beforeAll(() => {
+    item1 = {
+      name: "Sword of Slashing",
+      durability: 50,
+      enhancement: 0,
+    };
+    item2 = {
+      name: "Dagger of stabbing",
+      durability: 50,
+      enhancement: 20,
+    };
+    item3 = {
+      name: "Dagger of stabbing",
+      durability: 50,
+      enhancement: 4,
+    };
+    item4 = {
+      name: "Dagger of stabbing",
+      durability: 50,
+      enhancement: 15,
+    };
+  });
+
+  it("Should return durability 100", () => {
+    expect(enhancer.repair(item1).durability).toEqual(100);
+  });
+
+  it("Should return successful enhancment", () => {
+    expect(enhancer.success(item1).enhancement).toBe(1);
+  });
+
+  it("Should return max enhancement level", () => {
+    expect(enhancer.success(item2).enhancement).toBe(20);
+  });
+
+  it("Should return failed enhancement -1 enhancement level", () => {
+    expect(enhancer.fail(item2).enhancement).toBe(19);
+  });
+
+  it("Should return failed enhancement -5 loss to durablity", () => {
+    expect(enhancer.fail(item3).durability).toBe(45);
+  });
+
+  it("Should return failed enhancement -15 loss to durablity", () => {
+    expect(enhancer.fail(item4).durability).toBe(35);
+  });
+});
