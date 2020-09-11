63d35c56b4f72744c2783d1a5db723f8e6aebf34
Josh Riddle <riddlej9879@yahoo.com>
2020-09-10 20:58:13 -0500

    Imroved testing documentation and improved tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,31 +5,51 @@ module.exports = {
   get,
 };
 
+// Takes an item and adds and enhancment level on success
 function success(item) {
   const maxEnhancement = 20;
+  let enhancement = item.enhancement;
   if (item.enhancement === maxEnhancement) {
     return { ...item };
-  } else if (item.enhancement < maxEnhancement) {
-    item.enhancement++;
+  }
+  if (item.enhancement < maxEnhancement && item.enhancement >= 0) {
+    enhancement = item.enhancement + 1;
   } else {
-    item.enhancement = 0;
+    enhancement = 0;
   }
 
-  return { ...item };
+  return { ...item, enhancement };
 }
 
+// Takes an item and modifies the durability or enhancement level on failure
+// Enhancement less than 15 reduces durability by 5
+// Enhancement greater than 15 reduces durability by 10
+// Enhancement greater than 16 reduces the enhancement level by 1 in addition to the durability loss
 function fail(item) {
-  if (item.enhancement < 5) {
-    item.durability = item.durability - 5;
-  } else if (item.enhancement < 16) {
-    item.durability = item.durability - 15;
-  } else if (item.enhancement > 16) {
-    item.enhancement--;
+  let enhancement = item.enhancement;
+  let durability = item.durability;
+  if (enhancement < 15) {
+    if (durability < 5) {
+      durability = 0;
+    } else {
+      durability -= 5;
+    }
+  }
+  if (item.enhancement >= 15) {
+    if (durability < 10) {
+      durability = 0;
+    } else {
+      durability -= 10;
+    }
+  }
+  if (item.enhancement > 16) {
+    enhancement -= 1;
   }
 
-  return { ...item };
+  return { ...item, enhancement, durability };
 }
 
+// Takes and item and increases the durability to 100
 function repair(item) {
   return { ...item, durability: 100 };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,51 +1,83 @@
 const enhancer = require("./enhancer.js");
 // test away!
 
-describe("enhancer", () => {
-  beforeAll(() => {
-    item1 = {
-      name: "Sword of Slashing",
-      durability: 50,
-      enhancement: 0,
-    };
-    item2 = {
-      name: "Dagger of stabbing",
-      durability: 50,
+describe("enhancer.js repair function test", () => {
+  // Descriptive And Meaningful Phrases
+  const sword = {
+    name: "Sword of slashing",
+    durability: 50,
+    enhancement: 0,
+  };
+  it("Tests repair function should return an item with 100 durability", () => {
+    expect(enhancer.repair(sword).durability).toEqual(100);
+  });
+});
+
+describe("enhancer.js success function test", () => {
+  const dagger = {
+    name: "Dagger of stabbing",
+    durability: 50,
+    enhancement: 5,
+  };
+  const club = {
+    name: "Club of Beating",
+    durability: 50,
+    enhancement: -11,
+  };
+  it("Tests success function should return an item with enhancement increased by one", () => {
+    expect(enhancer.success(dagger).enhancement).toBe(6);
+    // Verifies items below level 0 are set to 0
+    expect(enhancer.success(club).enhancement).toBe(0);
+  });
+
+  it("Tests success function will not raise an item above the maxEnhament level of 20", () => {
+    const stick = {
+      name: "Stick of poking",
+      durability: 100,
       enhancement: 20,
     };
-    item3 = {
-      name: "Dagger of stabbing",
-      durability: 50,
-      enhancement: 4,
-    };
-    item4 = {
-      name: "Dagger of stabbing",
-      durability: 50,
-      enhancement: 15,
-    };
+    expect(enhancer.success(stick).enhancement).toBe(20);
+  });
+});
+
+describe("enhancer.js fail function test", () => {
+  const cloak = {
+    name: "Cloak of Covering",
+    durability: 50,
+    enhancement: 14,
+  };
+  const rope = {
+    name: "Rope of Tying",
+    durability: 50,
+    enhancement: 15,
+  };
+  const rock = {
+    name: "Rock of throwing",
+    durability: 100,
+    enhancement: 17,
+  };
+  const shield = {
+    name: "Shield of blocking",
+    durability: 4,
+    enhancement: 10,
+  };
+
+  it("Failure function should return -5 durability on items with enhancement level less than 15", () => {
+    expect(enhancer.fail(cloak).durability).toBe(45);
+    // Verifies enhancement remains unchanged
+    expect(enhancer.fail(cloak).enhancement).toBe(14);
+    // Verifies durablity will not drop below 0
+    expect(enhancer.fail(shield).durability).toBe(0);
   });
 
-  it("Should return durability 100", () => {
-    expect(enhancer.repair(item1).durability).toEqual(100);
+  it("Failure function should return -10 durability on items with enhancement level higher than 15", () => {
+    expect(enhancer.fail(rope).durability).toBe(40);
+    // Verifies enhancement remains unchanged
+    expect(enhancer.fail(rope).enhancement).toBe(15);
   });
 
-  it("Should return successful enhancment", () => {
-    expect(enhancer.success(item1).enhancement).toBe(1);
-  });
-
-  it("Should return max enhancement level", () => {
-    expect(enhancer.success(item2).enhancement).toBe(20);
-  });
-
-  it("Should return failed enhancement -1 enhancement level", () => {
-    expect(enhancer.fail(item2).enhancement).toBe(19);
-  });
-
-  it("Should return failed enhancement -5 loss to durablity", () => {
-    expect(enhancer.fail(item3).durability).toBe(45);
-  });
-
-  it("Should return failed enhancement -15 loss to durablity", () => {
-    expect(enhancer.fail(item4).durability).toBe(35);
+  it("Failure function should return -10 durability and -1 to enhancement on items higher than enhancement 16", () => {
+    expect(enhancer.fail(rock).durability).toBe(90);
+    expect(enhancer.fail(rock).enhancement).toBe(16);
   });
 });
